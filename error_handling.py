5f023913457cdee6deb2ddd20084f996ee6bb99d
serge <kserge2001@yahoo.fr>
2023-10-31 19:44:29 -0500

    python code

---
+++ error_handling.py
@@ -0,0 +1,13 @@
+
+"""try:
+    prin(a)
+except Exception as e:
+    print(e)       
+
+print("hello")
+"""
+try:
+    num = int(input("Please enter a number: "))
+    print(num)
+except:
+    print("Invalid entry please try again")
